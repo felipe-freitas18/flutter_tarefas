3307d7acc9629850126a2da5f391eac70b8a0d34
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2025-07-27 09:53:18 -0300

    feat: corrigindo configurações do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,29 @@
-import 'package:alura_flutter_curso_1/screens/tela_inicial.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tarefas/screens/tela_inicial.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
+
   // This widget is the root of your application.
   @override
-  bool opacidade = true;
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: InitialScreen(),
+      home: const InitialScreen(),
     );
   }
 }
-
-
-
--- lib/screens/tela_inicial.dart
+++ lib/screens/tela_inicial.dart
@@ -1,28 +1,28 @@
-import 'package:alura_flutter_curso_1/components/tasks.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tarefas/components/tasks.dart';
 
 class InitialScreen extends StatefulWidget {
-  const InitialScreen({Key? key}) : super(key: key);
+  const InitialScreen({super.key});
 
   @override
   State<InitialScreen> createState() => _InitialScreenState();
 }
 
 class _InitialScreenState extends State<InitialScreen> {
-  @override
   bool opacidade = true;
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter: Primeiros Passos'),
-        leading: Icon(Icons.add_task),
+        leading: const Icon(Icons.add_task),
       ),
       body: AnimatedOpacity(
         opacity: opacidade ? 1.0 : 0.0,
         duration: const Duration(milliseconds: 500),
         child: Container(
-          color: Color.fromARGB(255, 208, 221, 237),
+          color: const Color.fromARGB(255, 208, 221, 237),
           child: ListView(
             children: const [
               Padding(
