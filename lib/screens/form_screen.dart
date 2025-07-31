973902429f30330027ed66c396434d46997aa1c6
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2025-07-31 15:55:46 -0300

    feat: iniciando construção da tela de formulário

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -11,8 +11,31 @@ class _FormScreenState extends State<FormScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Nova Tarefa"),
+      appBar: AppBar(title: const Text("Nova Tarefa")),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          height: MediaQuery.of(context).size.height * 80,
+          width: MediaQuery.of(context).size.width * 80,
+          decoration: BoxDecoration(
+            color: Colors.black26,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(width: 3),
+          ),
+          child: Column(
+            children: [
+              TextFormField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Nome",
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
