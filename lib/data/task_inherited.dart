06a7438bcc5916d5064d557bd63157cfa9a7ff43
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2025-08-23 16:56:14 -0300

    feat: criando um taskInherited para guardar os dados

---
+++ lib/data/task_inherited.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tarefas/components/tasks.dart';
+
+class TaskInherited extends InheritedWidget {
+  TaskInherited({Key? key, required Widget child})
+    : super(key: key, child: child);
+
+  final List<Tasks> taskList = [
+    Tasks('Estudar Flutter', 'assets/images/flutter.png', 3),
+    Tasks('Andar de Bike', 'assets/images/bike.webp', 2),
+    Tasks('Ler 50 páginas', 'assets/images/ler.jpg', 1),
+    Tasks('Meditar', 'assets/images/meditar.jpeg', 4),
+    Tasks('Jogar', 'assets/images/jogar.jpg', 1),
+  ];
+
+  void newTask(String nome, String foto, int dificuldade) {
+    taskList.add(Tasks(nome, foto, dificuldade));
+  }
+
+  static TaskInherited of(BuildContext context) {
+    final TaskInherited? result = context.dependOnInheritedWidgetOfExactType();
+    assert(result != null, 'No TaskInherited found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    throw UnimplementedError();
+  }
+}
